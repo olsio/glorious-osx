39267da1c76703ca6fb8cf4b4c8b157e2724d523
Oliver Schneider <os@ols.io>
2019-03-21 08:23:30 +0100

    Initial Commit

---
+++ glorious-osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  glorious-osx
+//
+//  Created by Oliver Schneider on 21.03.19.
+//  Copyright © 2019 Oliver Schneider. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ glorious-osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  glorious-osx
+//
+//  Created by Oliver Schneider on 21.03.19.
+//  Copyright © 2019 Oliver Schneider. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
